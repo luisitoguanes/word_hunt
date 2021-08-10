4ab70f2508bc4e9d8a5f1cb62080c319b925a6d3
Luis Guanes <luis@toky.co>
2021-08-10 19:01:00 -0400

    Full proyect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { Container, withStyles } from '@material-ui/core';
+import Switch from '@material-ui/core/Switch';
+import { grey } from '@material-ui/core/colors';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Definitions from './components/Definitions/Definitions';
+import Header from './components/Header/Header';
 
 function App() {
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+  const [category, setCategory] = useState("en_US");
+  const [LightMode, setLightMode] = useState(false);
+
+  const DarkMode = withStyles({
+    switchBase: {
+      color: grey[300],
+      '&$checked': {
+        color: grey[500],
+      },
+      '&$checked + $track': {
+        backgroundColor: grey[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
+
+  useEffect(() => {
+    const dictionaryApi = async () => {
+      try {
+        const data = word ? await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`): {"data":[]};
+
+        setMeanings(data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    dictionaryApi();
+  }, [word, category])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <div className="App" style={{ height: '100vh', backgroundColor: LightMode ? "#fff" : '#282c34', color: LightMode ? "black" : 'white', transition: "all 0.5s linear" }}>
+    <Container maxWidth="md" style={{ display: "flex", flexDirection: "column", height: '100vh', justifyContent: 'space-evenly' }}>
+      <div style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}>
+        <span>{LightMode ? "Dark" : "Light"} Mode</span>
+        <DarkMode checked={LightMode} onChange={() => setLightMode(!LightMode)} />
+      </div>
+      <Header category={category} setCategory={setCategory} word={word} setWord={setWord} LightMode={LightMode} />
+      {meanings && (<Definitions word={word} meanings={meanings} category={category} LightMode={LightMode} />)}
+    </Container>
+  </div>
+)}
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,45 @@
+import { MenuItem, TextField, ThemeProvider, unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core';
+import React from 'react';
+import './Header.css';
+import categories from '../../data/category'
+
+const Header = ({setCategory, category, word, setWord, LightMode}) => {
+    const darkTheme = createMuiTheme({
+        palette: {
+            primary: {
+                main: LightMode?"#000":'#fff'
+            },
+            type: LightMode?"light":'dark',
+        },
+    });
+
+    const handleChange = (language) => {
+        setCategory(language);
+        setWord("");
+    };
+
+    return (
+        <div className="header">
+            <span className="title">{word ? word : "Word Hunt"}</span>
+            <div className="inputs">
+                <ThemeProvider theme={darkTheme}>
+                    <TextField id="standard-basic" className="search" label="Search a Word" value={word} onChange={(e) => setWord(e.target.value)}/>
+                    <TextField
+                        className="select"
+                        select
+                        label="Language"
+                        value={category}
+                        onChange={(e) => handleChange(e.target.value)}>
+                        {categories.map((option) => (
+                                <MenuItem key={option.label} value={option.label}>
+                                {option.value}
+                                </MenuItem>
+                            ))}
+                    </TextField>
+                </ThemeProvider>
+            </div>
+        </div>
+    )
+}
+
+export default Header
